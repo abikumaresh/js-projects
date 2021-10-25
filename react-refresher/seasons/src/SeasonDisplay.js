151bdb056c7fea78fdcd36c75e2204ab450b6468
Abirami Kumaresh <abisekar@gmail.com>
2021-10-25 20:59:46 +0530

    Add SeasonDisplay component

--- react-refresher/seasons/src/SeasonDisplay.js
+++ react-refresher/seasons/src/SeasonDisplay.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import './SeasonDisplay.css'
 
-const SeasonDisplay = () => {
-    return <div>
-        In Season Display
-    </div>
+const seasonConfig = {
+    summer : {
+        text : "Lets hit the beach!",
+        iconName : "sun"
+    },
+    winter : {
+        text : "Burr, it's chilly",
+        iconName : "snowflake"
+    }
+}
+
+const getSeason = (lat, month) => {
+    console.log(month)
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter'
+    } else {
+        return lat > 0 ? 'winter' : 'summer'
+    }
+}
+
+const SeasonDisplay = (props) => {
+    console.log(props.lat)
+    const season = getSeason(props.lat, new Date().getMonth())
+    console.log(season)
+    const {text, iconName} = seasonConfig[season]
+
+    return (<div>
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive icon ${iconName}` } />
+            <h1>{text}</h1>
+            <i className={`icon-right massive icon ${iconName}` }/>
+        </div>       
+
+    </div>)
 }
 
 export default SeasonDisplay
--- react-refresher/seasons/src/index.js
+++ react-refresher/seasons/src/index.js
@@ -1,19 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
 class App extends React.Component {
 
-    
-    render () {
+    state = {lat: null, errorMessage: null}
+ 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
+            position => {
+                console.log(position)
+                this.setState( {lat : position.coords.latitude})
+            },
+            err => {
+                console.log(err)
+                this.setState({errorMessage: err.message})
+            }
         )
-        
-        return <div> 
-            Hi There 
-        </div>
+    }
+
+    render () {
+
+        if (this.state.lat && !this.state.errorMessage) {
+            return <div> <SeasonDisplay lat={this.state.lat} /></div>
+        }
+        if (!this.state.lat && this.state.errorMessage) {
+            return <div> Error : {this.state.errorMessage} </div>
+        }
+
+        return <Spinner message="Give permission to location app.."/>
     }      
 }
 
